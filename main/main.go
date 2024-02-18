cf156b34aee523ec3ade97db71e26c32b17da5da
asadiqui <sadiquiabdelilah@gmail.com>
2024-02-18 14:39:48 +0100

    initializing the coding

---
+++ main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "quad/piscine"
+
+func main() {
+	// Testing QuadA function
+	piscine.QuadA(5, 3)
+	piscine.QuadA(5, 1)
+	piscine.QuadA(1, 1)
+	piscine.QuadA(1, 5)
+
+	// // Testing QuadB function
+	// piscine.QuadB(5, 3)
+	// piscine.QuadB(5, 1)
+	// piscine.QuadB(1, 1)
+	// piscine.QuadB(1, 5)
+
+	// // Testing QuadC function
+	// piscine.QuadC(5, 3)
+	// piscine.QuadC(5, 1)
+	// piscine.QuadC(1, 1)
+	// piscine.QuadC(1, 5)
+
+	// // Testing QuadD function
+	// piscine.QuadD(5, 3)
+	// piscine.QuadD(5, 1)
+	// piscine.QuadD(1, 1)
+	// piscine.QuadD(1, 5)
+
+	// // Testing QuadE function
+	// piscine.QuadE(5, 3)
+	// piscine.QuadE(5, 1)
+	// piscine.QuadE(1, 1)
+	// piscine.QuadE(1, 5)
+}
